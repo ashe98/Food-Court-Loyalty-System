38fe789a3eafd64ae72ea6897aa07091c36a46ce
nivash <s.nivash24@gmail.com>
2024-10-29 23:03:24 +0800

    ft: changes w.r.t pr

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 contract Marketplace {
     struct Product {
         uint256 id;
-        string name;
         address storeAddress;
         uint256 price;
         uint256 quantity;
         bool claimableOnce;
         bool groupClaimable;
+        uint256 minimumTierRequired;
         bool isActive;
     }
 
@@ -18,8 +18,6 @@ contract Marketplace {
     uint256 timestamp;
     uint256 quantity;
     }
-    // State variables
-    uint256 private nextProductId = 1; 
 
     //Modifiers
     modifier productExists(uint256 _productId) {
@@ -28,18 +26,11 @@ contract Marketplace {
     _;
     }
 
-    // modifier onlyStore() {
-    //reference to an instance of an external User contract, which is used to check if the store is registered by the user accessing the contract
-    //     require(userContract.stores(msg.sender).storeAddress == msg.sender, "Not a registered store");//checkd if the store is registered
-    //     _;
-    // }
-
-
-
     // TODO: store a mapping of address -> list of products claimed
     mapping(uint256 => Product) private products;
     mapping(address => mapping(uint256 => bool)) private hasUserClaimed; // user -> productId -> claimed
     mapping(address => ClaimRecord[]) private userClaimHistory;
+    uint256[] private allProductIds;
 
     event ProductAdded(uint256 indexed productId, address indexed store, string name, uint256 price);
     event ProductUpdated(uint256 indexed productId, uint256 quantity, uint256 price);
@@ -49,33 +40,35 @@ contract Marketplace {
 
     // TODO: Define method to add product
      function addProduct(
+        uint256 _productId,
         string memory _name,
         uint256 _price,
         uint256 _quantity,
         bool _claimableOnce,
-        bool _groupClaimable
-    ) external returns (uint256) {
+        bool _groupClaimable,
+        uint256 _minimumTierRequired
+    ) public returns (uint256) {
         require(_price > 0, "Price must be greater than 0");
         require(_quantity > 0, "Quantity must be greater than 0");
-        uint256 productId = nextProductId++;
         
-        products[productId] = Product({
-            id: productId,
-            name: _name,
+        products[_productId] = Product({
+            id: _productId,
             storeAddress: msg.sender,
             price: _price,
             quantity: _quantity,
             claimableOnce: _claimableOnce,
             groupClaimable: _groupClaimable,
+            minimumTierRequired: _minimumTierRequired,
             isActive: true
         });
-        emit ProductAdded(productId, msg.sender, _name, _price);
-        return productId;
+        allProductIds.push(_productId);
+        emit ProductAdded(_productId, msg.sender, _name, _price);
+        return _productId;
     }
 
     // TODO: Define method to get product by id
     function getProductById(uint256 _productId) 
-        external 
+        public
         view 
         productExists(_productId) 
         returns (Product memory) 
@@ -85,33 +78,22 @@ contract Marketplace {
 
     // TODO: Define method to get all products
     function getAllProducts() 
-        external 
+        public
         view 
         returns (Product[] memory) 
     {
-        // First, count the number of active products
-        uint256 activeCount = 0;
-        for (uint256 i = 1; i < nextProductId; i++) {
-            if (products[i].isActive) {
-                activeCount++;
-            }
+        Product[] memory allProducts = new Product[](allProductIds.length);
+        
+        for (uint256 i = 0; i < allProductIds.length; i++) {
+            allProducts[i] = products[allProductIds[i]];
         }
-        // Create an array of the activeCount size
-        Product[] memory allProducts = new Product[](activeCount);
-        // Fill the array with active products
-        uint256 currentIndex = 0;
-        for (uint256 i = 1; i < nextProductId; i++) {
-            if (products[i].isActive) {
-                allProducts[currentIndex] = products[i];
-                currentIndex++;
-            }
-        }
+        
         return allProducts;
     }
 
     // TODO: Define method to delete product
     function deleteProduct(uint256 _productId) 
-        external
+        public
         productExists(_productId) 
     {
         require(products[_productId].storeAddress == msg.sender, "Not product owner");
@@ -122,7 +104,7 @@ contract Marketplace {
 
     // TODO: Define public method to reduce quantity of product when purchased
     function reduceProductQuantity(uint256 _productId, uint256 _quantity) 
-        external 
+        public 
         productExists(_productId) 
     {
         require(products[_productId].quantity >= _quantity, "Not enough quantity");
@@ -131,20 +113,20 @@ contract Marketplace {
 
     //Method to get user claim history
     function getUserClaimHistory(address _user) 
-        external 
+        public 
         view 
         returns (ClaimRecord[] memory) 
     {
         return userClaimHistory[_user];
     }
 
-    //Methof to update product
+    //Method to update product
     function updateProduct(
         uint256 _productId,
         uint256 _price,
         uint256 _quantity
     ) 
-        external
+        public
         productExists(_productId) 
     {
         require(products[_productId].storeAddress == msg.sender, "Not product owner");
