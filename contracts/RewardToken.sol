12b5cfa0f5986a6b02cdb6f138197de6a5f225f6
Asher Everen <ashe98@gmail.com>
2024-10-31 16:33:39 +0800

    group transactions

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./models/Product.sol";
+import "./models/Group.sol";
+
 import "./Marketplace.sol";
 
 contract RewardToken is ERC20, Ownable {
@@ -26,6 +28,7 @@ contract RewardToken is ERC20, Ownable {
     mapping(address => bool) private isCustomer;
     mapping(address => mapping(address => uint256))
         private userLastTransactionTimestampForStore;
+    mapping(address => Group) private groups; // ongoing Group transactions, customer -> Group
 
     Marketplace private marketplace;
 
@@ -57,33 +60,7 @@ contract RewardToken is ERC20, Ownable {
             _;
             return;
         }
-        uint256 length = tokenBatches[customer].length;
-        uint256 expiredAmount = 0;
-        uint256[] memory expiredIndexes = new uint256[](length);
-
-        // first find expired token batches
-        for (uint256 i = 0; i < length; i++) {
-            if (tokenBatches[customer][i].expiration < block.timestamp) {
-                expiredAmount += tokenBatches[customer][i].amount;
-            } else {
-                expiredIndexes[i] = 1;
-            }
-        }
-
-        // update the token batches array for the customer
-        if (expiredAmount > 0) {
-            uint256 newLength = length - expiredAmount;
-            TokenBatch[] memory newTokenBatches = new TokenBatch[](newLength);
-            uint256 j = 0;
-            for (uint256 i = 0; i < length; i++) {
-                if (expiredIndexes[i] == 1) {
-                    newTokenBatches[j] = tokenBatches[customer][i];
-                    j++;
-                }
-            }
-            tokenBatches[customer] = newTokenBatches;
-            _burn(customer, expiredAmount); // burn the expired token amount
-        }
+        _burnExpiredTokens(customer);
         _;
     }
 
@@ -172,7 +149,7 @@ contract RewardToken is ERC20, Ownable {
         uint256 amount,
         uint256 transactionId,
         address storeAddress
-    ) public onlyOwner {
+    ) public onlyOwner burnExpiredTokens {
         uint256 expiration = block.timestamp + 4 * 6 weeks; // 6 months
         mintTokens(customer, amount, expiration, transactionId);
         userLastTransactionTimestampForStore[customer][storeAddress] = block
@@ -194,8 +171,86 @@ contract RewardToken is ERC20, Ownable {
             balanceOf(_msgSender()) < productToRedeem.price,
             "Customer does not have enough tokens to redeem"
         );
-        transfer(store, productToRedeem.price);
         marketplace.redeemProduct(productId, 1); // reduce product quantity
+        transfer(store, productToRedeem.price);
+    }
+
+    function createGroup() public burnExpiredTokens {
+        address customer = _msgSender();
+        require(
+            isCustomer[customer],
+            "Only customers can create a group transaction"
+        );
+        require(
+            groups[customer].originator == address(0),
+            "Customer already has an ongoing group transaction"
+        );
+        address[] memory members = new address[](5);
+        members[0] = customer;
+        groups[customer] = Group(customer, members, block.timestamp);
+    }
+
+    function joinGroup(address groupOriginator) public burnExpiredTokens {
+        address customer = _msgSender();
+        require(
+            isCustomer[customer],
+            "Only customers can join a group transaction"
+        );
+        require(
+            groups[groupOriginator].originator != address(0),
+            "Group does not exist"
+        );
+        require(groups[groupOriginator].members.length < 5, "Group is full");
+        for (uint256 i = 0; i < groups[groupOriginator].members.length; i++) {
+            require(
+                groups[groupOriginator].members[i] != customer,
+                "Customer is already in the group"
+            );
+        }
+        groups[groupOriginator].members.push(customer);
+    }
+
+    function makeGroupTransaction(
+        uint256 productId,
+        address store
+    ) public burnExpiredTokens {
+        address customer = _msgSender();
+        require(
+            isCustomer[customer],
+            "Only customers can initiate a group transaction"
+        );
+        require(
+            groups[customer].originator != address(0),
+            "Customer does not have an ongoing group transaction"
+        );
+        require(groups[customer].members.length > 1, "Group is empty");
+        if (block.timestamp - groups[customer].createdAt <= 30 minutes) {
+            deleteGroup(customer); // Delete group if expired
+            require(false, "Group transaction expired");
+        }
+        Product memory productToRedeem = marketplace.getProductById(productId);
+        require(
+            productToRedeem.groupClaimable,
+            "Product is not group claimable"
+        );
+        uint256 groupSize = groups[customer].members.length;
+        for (uint256 i = 0; i < groupSize; i++) {
+            address member = groups[customer].members[i];
+            _burnExpiredTokens(member);
+            require(
+                balanceOf(member) < productToRedeem.price / groupSize,
+                "Customer does not have enough tokens to redeem"
+            );
+        }
+
+        // Redeem product
+        marketplace.redeemProduct(productId, 1);
+        for (uint256 i = 0; i < groupSize; i++) {
+            address member = groups[customer].members[i];
+            transferFrom(member, store, productToRedeem.price / groupSize);
+        }
+
+        deleteGroup(customer);
     }
 
     //////////////////////////////////////////
@@ -214,6 +269,43 @@ contract RewardToken is ERC20, Ownable {
             TokenBatch(transactionId, amount, expiration)
         );
         _mint(customer, amount);
+    }
+
+    function _burnExpiredTokens(address customer) internal {
+        if (!isCustomer[customer]) {
+            return;
+        }
+        uint256 length = tokenBatches[customer].length;
+        uint256 expiredAmount = 0;
+        uint256[] memory expiredIndexes = new uint256[](length);
+
+        // first find expired token batches
+        for (uint256 i = 0; i < length; i++) {
+            if (tokenBatches[customer][i].expiration < block.timestamp) {
+                expiredAmount += tokenBatches[customer][i].amount;
+            } else {
+                expiredIndexes[i] = 1;
+            }
+        }
+
+        // update the token batches array for the customer
+        if (expiredAmount > 0) {
+            uint256 newLength = length - expiredAmount;
+            TokenBatch[] memory newTokenBatches = new TokenBatch[](newLength);
+            uint256 j = 0;
+            for (uint256 i = 0; i < length; i++) {
+                if (expiredIndexes[i] == 1) {
+                    newTokenBatches[j] = tokenBatches[customer][i];
+                    j++;
+                }
+            }
+            tokenBatches[customer] = newTokenBatches;
+            _burn(customer, expiredAmount); // burn the expired token amount
+        }
+    }
+
+    function deleteGroup(address customer) internal {
+        groups[customer] = Group(address(0), new address[](0), 0);
     }
 
     //////////////////////////////////////////
---
+++ contracts/models/Group.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+struct Group {
+    address originator;
+    address[] members;
+    uint256 createdAt;
+}
